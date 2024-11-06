ed2d75b2a29e8207e7eb6d8596ee8959b7b89abb
ChengyueLiu <17554334529@163.com>
2024-11-06 13:32:28 +0800

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+
+# Example: Download Oxford-IIIT Pet Dataset
+# !wget -P https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
+# !wget -P https://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
+# !tar -xzf images.tar.gz
+# !tar -xzf annotations.tar.gz
